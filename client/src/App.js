e747a603ff74964e1c07f727ab1476f11045fdca
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-23 13:57:05 -0500

    client/src/App.js updated: imports added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-boost';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -14,7 +11,6 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
