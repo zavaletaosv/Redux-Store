ec3241a54d145a229da3fb4d91b92b9ba1f1944a
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-24 13:28:26 -0500

    client/src/utils/reducers.js updated: comments removed

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -11,11 +11,8 @@ import {
   TOGGLE_CART,
 } from './actions';
 
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
 export const reducer = (state, action) => {
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // Your comment here
     case UPDATE_PRODUCTS:
       return {
         ...state,
@@ -34,8 +31,7 @@ export const reducer = (state, action) => {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // Your comment here
+
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -48,8 +44,6 @@ export const reducer = (state, action) => {
         }),
       };
 
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
@@ -86,8 +80,6 @@ export const reducer = (state, action) => {
         currentCategory: action.currentCategory,
       };
 
-    // TODO: Add a comment describing what the default case is for
-    // Your comment here
     default:
       return state;
   }
