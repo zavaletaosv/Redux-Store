af73bcb41df50d113ff06c08369cc40424c67508
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-24 13:40:52 -0500

    client/src/pages/Detail.js updated: imports modified

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/react-hooks';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
