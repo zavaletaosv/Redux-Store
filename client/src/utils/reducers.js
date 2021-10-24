91917b6c1aa234992c5a48d1c8923a47d4e978e0
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-24 13:35:50 -0500

    client/src/utils/reducers.js updated: refactored code in file

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,3 @@
-import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,7 +10,15 @@ import {
   TOGGLE_CART,
 } from './actions';
 
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: '',
+  cart: [],
+  cartOpen: false
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
@@ -85,6 +92,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducer;
